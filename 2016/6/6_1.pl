3aa8453c2d91cda52eea42cda7ccf149ce8a2e20
Paul King <pking@factset.com>
2016-12-06 11:30:12 -0500

    adding day 6 solution

---
+++ 2016/6/6_1.pl
@@ -0,0 +1,37 @@
+#! perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+die "usage: perl $0 <input> (part [1|2])" if (scalar @ARGV != 1 && scalar @ARGV != 2);
+
+my ($sInput, $sPart) = @ARGV;
+
+$sPart = 2 if(! defined $sPart);
+
+open (my $rInputHandle ,'<', $sInput) or die "couldn't open file for read, $!";
+my %hHash =();
+
+my $sWidth = 0;
+while(defined(my $sLine = <$rInputHandle>)){
+    chomp $sLine;
+    my @aChars = split //,$sLine;
+
+    $sWidth = scalar @aChars if($sWidth == 0);
+    for ( my $sI=0; $sI < $sWidth; $sI++){
+        my $sCount = 0;
+        my $sChar = $aChars[$sI];
+        $sCount = $hHash{$sI}{$sChar} if(exists $hHash{$sI}{$sChar});
+        $hHash{$sI}{$sChar} = $sCount +1;
+    }
+}
+
+
+for (my $sI=0; $sI<$sWidth;$sI++){
+    foreach my $sKey (sort { $hHash{$sI}{$b} <=> $hHash{$sI}{$a} } keys %{$hHash{$sI}}){
+        print $sKey;
+        last;
+    }
+}
